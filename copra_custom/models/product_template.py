c5a056b3d6c1ade1b8ad3ded4851cb64bb3581f2
Krutarth Buch <krutarthbuch1@gmail.com>
2022-09-20 18:20:00 +0530

    Improved logic for default internal reference.

--- copra_custom/models/product_template.py
+++ copra_custom/models/product_template.py
@@ -2,25 +2,13 @@ from odoo import api, models, fields
 
 
 class ProductTemplate(models.Model):
-	_inherit = "product.template"
+    _inherit = "product.template"
 
-
-	default_code = fields.Char('Internal Reference', compute='_compute_default_code', inverse='_set_default_code', store=True)
-
-
-	@api.model
-	def create(self,vals):
-		if vals is None:
-			vals = {}
-		if not vals.get('default_code', False):
-			vals['default_code'] = self.env["ir.sequence"].next_by_code('product.template.internal.ref') or _(' ')
-		return super(ProductTemplate,self).create(vals)
-
-
-
-
-
-
-
-
+    @api.model
+    def create(self, vals):
+        if vals is None:
+            vals = {}
+        if not vals.get('default_code', False):
+            vals['default_code'] = self.env["ir.sequence"].next_by_code('product.template.internal.ref') or _(' ')
+        return super(ProductTemplate, self).create(vals)
 
