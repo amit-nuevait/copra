308cd3c8da61a46ba61c1eee4c116d7857a218ab
“Accudoo” <amit@accudoo.com>
2022-11-08 12:11:14 +0530

    [COP-29] added analytic account to shipment to genrate analytic entries for deliveries made to customer

---
+++ procurement_mto_analytic/models/sale_order_line.py
@@ -0,0 +1,25 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _prepare_procurement_values(self, group_id=False):
+        res = super()._prepare_procurement_values(group_id)
+        if self.order_id.project_id and self.order_id.project_id.analytic_account_id:
+            res.update({"account_analytic_id": self.order_id.project_id.analytic_account_id.id})
+        else:
+            res.update({"account_analytic_id": self.order_id.analytic_account_id.id})
+        return res
+
+    def _purchase_service_prepare_line_values(self, purchase_order, quantity=False):
+        res = super()._purchase_service_prepare_line_values(
+            purchase_order=purchase_order, quantity=quantity
+        )
+        if self.order_id.project_id and self.order_id.project_id.analytic_account_id:
+            res.update({"account_analytic_id": self.order_id.project_id.analytic_account_id.id})
+        else:
+            res.update({"account_analytic_id": self.order_id.analytic_account_id.id})
+        return res
